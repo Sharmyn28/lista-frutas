1396a6d8fae09cd47b3f5d51004b3d2ab40fbcfb
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-07-13 17:07:13 -0500

    Ejercicio frutas con html css y js

---
+++ js/main.js
@@ -0,0 +1,71 @@
+var productos = [];
+var records = document.getElementById('records');
+
+// Constructor para generar un nuevo producto
+function Producto(compra) {
+  this.compra = compra.toLowerCase();
+  this.producttID = (productos.length + 1);
+};
+
+//Método para imprimir un producto en html
+Producto.prototype.toHTML = function () {
+  var html = '';
+  html += this.compra.toUpperCase() + '<br>';
+  html += '<br><br>';
+  return html;
+}
+
+//Función que une todas las compras guardadas en el array productos
+function mergeHTML (){
+  var html = '';
+  for (var i=0; i<productos.length; i++){
+    html += productos[i].toHTML();
+  }
+  return html;
+}
+
+//función que imprime un producto luego de ingresarlo
+function printHTML (html){
+  records.innerHTML = '';
+  records.innerHTML = html;
+}
+
+// Cuando hacen click en el boton de nueva compra, crea una nueva compra y la añade al array de productos
+   //se hizo cambio en el formato del prompt
+var addCompra = document.getElementById('nuevacompra');
+addCompra.onclick = function() {
+  //var compra = prompt('Ingrese su nueva compra');
+
+  
+
+  swal({
+            title: "¿Que fruta desea agregar?",
+            text: "Ingrese el nombre de la fruta a agregar",
+            type: "input",
+            showCancelButton: true,
+            closeOnConfirm: false,
+            animation: "slide-from-top",
+            inputPlaceholder: "Write something"
+        },
+            function(inputValue){
+              if (inputValue === false) return false;
+
+              if (inputValue === "") {
+                  swal.showInputError("Debes ingresar una fruta");
+                  return false
+              }
+            var compra = inputValue;
+
+            swal("Perfecto", "Escribiste: " + compra, "success");
+                var product  = new Producto (compra);
+                productos.push(product);
+                printHTML(product.toHTML());                               
+      });
+};
+
+
+// al hacerle click a mostrar todas las compras, imprime todas las compras en el html. ESTA ESTÁ BUENA, NO CAMBIAR NADA
+var printAll = document.getElementById('print');
+printAll.onclick = function() {
+  printHTML(mergeHTML());
+}
